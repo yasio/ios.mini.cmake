6abdc1a956c9aace367ff4187bd6eb115761d95a
HALX99 <halx99@live.com>
2020-11-07 14:46:43 +0800

    Update ios.mini.cmake

--- ios.mini.cmake
+++ ios.mini.cmake
@@ -1,9 +1,7 @@
 # The workaround for solve try_compile failed with code sign
 # since cmake-3.18.2, not required
 # everyting for cmake toolchain config before project(xxx) is better
-
 set(CMAKE_SYSTEM_NAME "iOS" CACHE STRING "The CMake system name for iOS")
-
 set(CMAKE_TRY_COMPILE_PLATFORM_VARIABLES
     "CMAKE_XCODE_ATTRIBUTE_CODE_SIGNING_REQUIRED"
     "CMAKE_XCODE_ATTRIBUTE_CODE_SIGNING_ALLOWED")
